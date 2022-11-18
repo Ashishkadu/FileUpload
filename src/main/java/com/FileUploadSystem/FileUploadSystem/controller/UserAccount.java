8a6ad73541e6b0d7dcabe66f04131a4e43a6efc1
Ashishkadu <110523193+Ashishkadu@users.noreply.github.com>
2022-11-18 06:22:51 +0530

    Initial Commit

---
+++ src/main/java/com/FileUploadSystem/FileUploadSystem/controller/UserAccount.java
@@ -0,0 +1,45 @@
+package com.FileUploadSystem.FileUploadSystem.controller;
+
+import com.FileUploadSystem.FileUploadSystem.entity.DocumentInfo;
+import com.FileUploadSystem.FileUploadSystem.model.UserDTO;
+import com.FileUploadSystem.FileUploadSystem.service.DocumentUploadServiceImpl;
+import com.FileUploadSystem.FileUploadSystem.service.UserAccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.FileUploadSystem.FileUploadSystem.service.UserAccountServiceInterface;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+@RestController
+public class UserAccount {
+
+    @Autowired
+    private UserAccountServiceImpl userAccountServiceInterface;
+
+    @Autowired
+    private DocumentUploadServiceImpl documentUploadService;
+
+
+    @PostMapping(value = "/register")
+    public String addUser(@RequestBody UserDTO user)
+    {
+        String result = userAccountServiceInterface.addUser(user);
+        return result;
+
+    }
+
+    @PostMapping(value = "/login")
+    public String loginUser(@RequestBody UserDTO user)
+    {
+        String result  = userAccountServiceInterface.getUserDetail(user);
+        return result;
+    }
+
+
+
+
+}
